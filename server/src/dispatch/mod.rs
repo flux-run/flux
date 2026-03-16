66ff12a79c0e9402837c48f3c5d072912bc0317e
Shashi Sharma <shashis339@gmail.com>
2026-03-16 09:10:14 +0530

    refactor: remove in-process dispatch implementations and related module exports

--- server/src/dispatch/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod api_impl;
-pub mod de_impl;
-pub mod runtime_impl;
-
-pub use api_impl::InProcessApiDispatch;
-pub use de_impl::InProcessDataEngineDispatch;
-pub use runtime_impl::InProcessRuntimeDispatch;
